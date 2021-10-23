10192e416bd84f6b1b03392637ecd7d403e2f601
Roxana Gnes <roxana.gnes@stud.hslu.ch>
2021-10-23 20:02:24 +0200

    setActiveDay and eventColors implemented

--- src/components/CalendarEntry.vue
+++ src/components/CalendarEntry.vue
@@ -15,10 +15,12 @@
         </select>
         <div class="text-center mt-3">
           <span
-            v-for="i in 5"
-            :key="i"
-            class="d-inline-block alert alert-primary m-0 me-2 square"
+            v-for="color in eventColors"
+            :key="color"
+            class="d-inline-block alert m-0 me-2 square"
+            :class="eventColorClasses(color)"
             role="button"
+            @click="setEventColor(color)"
           >
           </span>
         </div>
@@ -36,9 +38,33 @@
 import Store from "../store.js";
 export default {
   name: "CalendarEntry",
+  data() {
+    return {
+      eventColors: ["primary", "success", "warning", "info", "danger"],
+      event: {
+        title: "",
+        color: "primary",
+        priority: 0,
+      },
+    };
+  },
   computed: {
     activeDayName() {
       return Store.getters.activeDay().fullName;
+    },
+  },
+  methods: {
+    eventColorClasses(eventColor) {
+      return [
+        "alert-" + eventColor,
+        this.event.color === eventColor
+          ? "border border-" + this.event.color
+          : "",
+      ];
+    },
+
+    setEventColor(eventColor) {
+      this.event.color = eventColor;
     },
   },
 };
--- src/store.js
+++ src/store.js
@@ -15,34 +15,38 @@ const getters = {
 };
 
 const mutations = {
-    editEvent(dayId, eventTitle) {
-        // Alle edit-Attribute auf false setzen, damit immer nur ein Event bearbeitet werden kann
-        state.calendarWeekData.map((dayObj) => {
-            dayObj.events.map((event) => (event.edit = false));
-        });
-        // Setze das entsprechende edit-Attribut auf true
-        const dayObj = state.calendarWeekData.find((day) => day.id === dayId);
-        const eventObj = dayObj.events.find((event) => event.title === eventTitle);
-        eventObj.edit = true;
-    },
-    deleteEvent(dayId, eventTitle) {
-        const dayObj = state.calendarWeekData.find((day) => day.id === dayId);
-        const eventIndex = dayObj.events.findIndex((event) => event.title === eventTitle);
-        dayObj.events.splice(eventIndex, 1);
-    },
-    updateEvent(dayId, oldEventTitle, newEvent) {
-        newEvent.title = newEvent.title !== "" ? newEvent.title : oldEventTitle;
-        const dayObj = state.calendarWeekData.find((day) => day.id === dayId);
-        const eventObj = dayObj.events.find((event) => event.title === oldEventTitle);
+        editEvent(dayId, eventTitle) {
+            // Alle edit-Attribute auf false setzen, damit immer nur ein Event bearbeitet werden kann
+            state.calendarWeekData.map((dayObj) => {
+                dayObj.events.map((event) => (event.edit = false));
+            });
+            // Setze das entsprechende edit-Attribut auf true
+            const dayObj = state.calendarWeekData.find((day) => day.id === dayId);
+            const eventObj = dayObj.events.find((event) => event.title === eventTitle);
+            eventObj.edit = true;
+        },
+        deleteEvent(dayId, eventTitle) {
+            const dayObj = state.calendarWeekData.find((day) => day.id === dayId);
+            const eventIndex = dayObj.events.findIndex((event) => event.title === eventTitle);
+            dayObj.events.splice(eventIndex, 1);
+        },
+        updateEvent(dayId, oldEventTitle, newEvent) {
+            newEvent.title = newEvent.title !== "" ? newEvent.title : oldEventTitle;
+            const dayObj = state.calendarWeekData.find((day) => day.id === dayId);
+            const eventObj = dayObj.events.find((event) => event.title === oldEventTitle);
 
-        eventObj.title = newEvent.title;
-        eventObj.priority = Number(newEvent.priority);
-        eventObj.edit = false;
-    },
-};
-
-export default {
-    state: readonly(state),
-    getters,
-    mutations,
-};
+            eventObj.title = newEvent.title;
+            eventObj.priority = Number(newEvent.priority);
+            eventObj.edit = false;
+        },
+        setActiveDay(dayId) {
+            state.calendarWeekData.map((dayObj) => {
+                dayObj.id === dayId ? (dayObj.active = true) : (dayObj.active = false);
+            });
+        },
+    };
+    export default {
+        state: readonly(state),
+        getters,
+        mutations,
+    };
