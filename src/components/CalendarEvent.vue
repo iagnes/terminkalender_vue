ff25961a5d9a189ea903710370af7c32b0fa5ff9
Roxana Gnes <roxana.gnes@stud.hslu.ch>
2021-10-23 19:32:44 +0200

    update Events implemented

--- src/components/CalendarEvent.vue
+++ src/components/CalendarEvent.vue
@@ -1,32 +1,60 @@
 <template>
   <div id="calendar-event">
     <div class="alert text-center" :class="alertColor">
-      <div>
-        <!-- <strong>{{ priorityDisplayName }}</strong> -->
-        <slot name="eventPriority" :priorityDisplayName="priorityDisplayName"
-          ><strong>{{ priorityDisplayName }}</strong></slot
+      <!-- Template für den Fall, dass das Event nicht bearbeitet wird -->
+      <div v-if="!event.edit">
+        <div>
+          <!-- <strong>{{ priorityDisplayName }}</strong> -->
+          <slot name="eventPriority" :priorityDisplayName="priorityDisplayName"
+            ><strong>{{ priorityDisplayName }}</strong></slot
+          >
+        </div>
+
+        <!-- <div>{{ event.title }}</div> -->
+
+        <slot :event="event"
+          ><div>{{ event.title }}</div></slot
         >
+        <div>
+          <i class="fas fa-edit me-2" role="button" @click="editEvent()"></i>
+          <i class="far fa-trash-alt" role="button" @click="deleteEvent()"></i>
+        </div>
       </div>
-
-      <!-- <div>{{ event.title }}</div> -->
-
-      <slot :event="event"
-        ><div>{{ event.title }}</div></slot
-      >
-      <div>
-        <i class="fas fa-edit me-2" role="button"></i>
-        <i class="far fa-trash-alt" role="button"></i>
+      <div v-else>
+        <input
+          type="text"
+          class="form-control"
+          ref="newEventTitleInput"
+          :placeholder="event.title"
+          @input="setNewEventTitle($event)"
+        />
+        <select class="form-select mt-2" v-model="newEventPriority">
+          <option value="-1">Hoch</option>
+          <option value="0">Mittel</option>
+          <option value="1">Tief</option>
+        </select>
+        <hr />
+        <i class="fas fa-check" @click="updateEvent()"></i>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Store from "../store.js";
 export default {
   name: "CalendarEvent",
   props: {
     event: Object,
+    day: Object,
   },
+  data() {
+    return {
+      newEventTitle: "",
+      newEventPriority: this.event.priority,
+    };
+  },
+
   computed: {
     priorityDisplayName() {
       switch (this.event.priority) {
@@ -41,6 +69,27 @@ export default {
     },
     alertColor() {
       return "alert-" + this.event.color;
+    },
+  },
+  methods: {
+    deleteEvent() {
+      Store.mutations.deleteEvent(this.day.id, this.event.title);
+    },
+    editEvent() {
+      Store.mutations.editEvent(this.day.id, this.event.title);
+      // Auf die Template Refs zugreifen
+      this.$nextTick(() => {
+        this.$refs.newEventTitleInput.focus();
+      });
+    },
+    setNewEventTitle(event) {
+      this.newEventTitle = event.target.value;
+    },
+    updateEvent() {
+      Store.mutations.updateEvent(this.day.id, this.event.title, {
+        title: this.newEventTitle,
+        priority: this.newEventPriority,
+      });
     },
   },
 };
--- src/store.js
+++ src/store.js
@@ -1,16 +1,46 @@
-import {calendarWeekData} from "./seed";
-import { reactive, readonly } from "vue";
- 
+import {
+    calendarWeekData
+} from "./seed";
+import {
+    reactive,
+    readonly
+} from "vue";
+
 const state = reactive({
     calendarWeekData,
 });
- 
+
 const getters = {
     activeDay: () => state.calendarWeekData.find((day) => day.active),
 };
- 
-const mutations = {};
- 
+
+const mutations = {
+    editEvent(dayId, eventTitle) {
+        // Alle edit-Attribute auf false setzen, damit immer nur ein Event bearbeitet werden kann
+        state.calendarWeekData.map((dayObj) => {
+            dayObj.events.map((event) => (event.edit = false));
+        });
+        // Setze das entsprechende edit-Attribut auf true
+        const dayObj = state.calendarWeekData.find((day) => day.id === dayId);
+        const eventObj = dayObj.events.find((event) => event.title === eventTitle);
+        eventObj.edit = true;
+    },
+    deleteEvent(dayId, eventTitle) {
+        const dayObj = state.calendarWeekData.find((day) => day.id === dayId);
+        const eventIndex = dayObj.events.findIndex((event) => event.title === eventTitle);
+        dayObj.events.splice(eventIndex, 1);
+    },
+    updateEvent(dayId, oldEventTitle, newEvent) {
+        newEvent.title = newEvent.title !== "" ? newEvent.title : oldEventTitle;
+        const dayObj = state.calendarWeekData.find((day) => day.id === dayId);
+        const eventObj = dayObj.events.find((event) => event.title === oldEventTitle);
+
+        eventObj.title = newEvent.title;
+        eventObj.priority = Number(newEvent.priority);
+        eventObj.edit = false;
+    },
+};
+
 export default {
     state: readonly(state),
     getters,
